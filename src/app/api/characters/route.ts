453b15f4350155e75ef20737baa5674f98ddfc71
Gizmoux <nicolas.deriemacker@hotmail.fr>
2025-03-25 15:35:06 +0100

    update api routes

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export const GET = async () => {
-	try {
-		const response = await fetch(
-			'https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=o6GAkUgopwiunWCT'
-		);
+  try {
+    const response = await fetch(
+      "https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=o6GAkUgopwiunWCT"
+    );
 
-		if (!response.ok) {
-			throw new Error('Failed to fetch data from external API');
-		}
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from external API");
+    }
 
-		const data = await response.json();
-		return NextResponse.json(data);
-	} catch (error) {
-		return NextResponse.json({ error: error.message });
-	}
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: error });
+  }
 };
