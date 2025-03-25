4cca7c74153df1ae45544a544bd4f817d1a5536b
Gizmoux <nicolas.deriemacker@hotmail.fr>
2025-03-25 15:38:36 +0100

    Update navbar from home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Characters from "./characters/page";
-import Navbar from "./Navbar/page";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Characters />
     </main>
   );
